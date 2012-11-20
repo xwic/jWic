c91b68efa8088caf408432e54062cbb4e598ff7f
bogdan <bogdan.bugarschi@netapp.com>
2012-11-20 11:57:29 +0200

    Added minimize and maximize options to Window

--- jWic/src/de/jwic/controls/Window.js
+++ jWic/src/de/jwic/controls/Window.js
@@ -11,7 +11,7 @@
 	afterUpdate: function(element) {
 		
 		var win = JWic.controls.Window.getWindow('win_${control.controlID}_div');	
-
+		
 				
 		win.dialog('option',{
 			#if($control.title) title: "$escape.escapeJavaScript($control.title)", #else title: '', #end
@@ -26,7 +26,7 @@
 			close: function () {
 				JWic.fireAction("$control.controlID", "close", "");	
 			},
-			
+	
 			draggable: #if($control.draggable) true, #else false, #end
 			resizable : #if($control.resizable) true, #else false, #end
 			modal: #if($control.modal) true, #else false, #end
@@ -34,12 +34,117 @@
 			
 			
 		});
+		
+		console.log(win.dialog('option'));
 //		minimizable : #if($control.minimizable) true, #else false,#end
 //		maximizable : #if($control.maximizable) true, #else false, #end
 		
 		win.dialog('open');		
+//		win.bind('updateIcons',function(event){
+//			var dialog = jQuery(this);
+//			var visible = dialog.is(':visible');
+//			
+//			if(!visible){				
+//				jQuery('#'+JQryEscape('${control.controlID}')+'_maximize').find('span').removeClass('ui-icon-newwin');
+//				jQuery('#'+JQryEscape('${control.controlID}')+'_maximize').find('span').addClass('ui-icon-extlink');
+//				jQuery('#'+JQryEscape('${control.controlID}')+'_minimize').find('span').removeClass('ui-icon-minusthick')
+//				jQuery('#'+JQryEscape('${control.controlID}')+'_minimize').find('span').addClass('ui-icon-extlink')
+//				
+//			}else{
+//				jQuery('#'+JQryEscape('${control.controlID}')+'_maximize').find('span').addClass('ui-icon-newwin')
+//				jQuery('#'+JQryEscape('${control.controlID}')+'_maximize').find('span').removeClass('ui-icon-extlink')
+//				jQuery('#'+JQryEscape('${control.controlID}')+'_minimize').find('span').addClass('ui-icon-minusthick')
+//				jQuery('#'+JQryEscape('${control.controlID}')+'_minimize').find('span').removeClass('ui-icon-extlink')
+//				
+//			}
+//		});
 		
+		var titlebar = win.parents('.ui-dialog').find('.ui-dialog-titlebar');	
+		//minimize
+		jQuery('<a href="#" id="${control.controlID}_minimize" role="button" class="ui-corner-all ui-dialog ui-dialog-titlebar-min"><span class="ui-icon ui-icon-minusthick">minimize</span></button>')
+			.appendTo(titlebar)
+			.mouseover(function(){
+				jQuery(this).addClass('ui-state-hover');
+			})
+			.mouseout(function(){
+				jQuery(this).removeClass('ui-state-hover');
+			})
+			.click(function() {
+				if(jQuery.data(win,'isMaximized')){
+					jQuery.data(win,'isMaximized',false);
+					var dialogParent = win.parent();					
+					var width  = jQuery.data(win,'originalSize').width;
+					var height = jQuery.data(win,'originalSize').height;					 
+					dialogParent.offset(jQuery.data(win,'originalPosition'));					
+					dialogParent.width(width);
+					dialogParent.height(height);
+				}
+				
+				if(!jQuery.data(win,'isMinimized')){
+					var dialogParent = win.parent();						
+					jQuery.data(win,'isMinimized',true);					
+					jQuery.data(win,'minOriginalSize',{width:dialogParent.width(),height:dialogParent.height()});
+					
+					var titlebar = dialogParent.find('.ui-dialog-titlebar');
+					var dialogPadding = (dialogParent.outerHeight(true) - dialogParent.innerHeight());
+					
+					dialogParent.width(titlebar.width()+dialogPadding);
+					dialogParent.height(titlebar.height()+dialogPadding);
+					win.hide();
+					
+				}else{
+					var dialogParent = win.parent();						
+					jQuery.data(win,'isMinimized',false);
+					dialogParent.width(jQuery.data(win,'minOriginalSize').width);
+					dialogParent.height(jQuery.data(win,'minOriginalSize').height);
+					win.show();
+					
+				}
+				
+//				win.trigger({type:'updateIcons',action:'minimize'});
+				//custom event for minimize
+				win.trigger({type:'minimize'});
+			});
 
+		
+		//maximize
+		jQuery('<a href="#" id="${control.controlID}_maximize" role="button" class="ui-corner-all ui-dialog ui-dialog-titlebar-max"><span class="ui-icon ui-icon-newwin">maximize</span></button>')
+		.appendTo(titlebar)
+		.mouseover(function(){
+			jQuery(this).addClass('ui-state-hover');
+		})
+		.mouseout(function(){
+			jQuery(this).removeClass('ui-state-hover');
+		})
+		.click(function() {
+			(function(){
+				if(!jQuery.data(win,'isMaximized')){				
+					jQuery.data(win,'isMaximized',true);
+					var dialogParent = win.parent();
+					jQuery.data(win,'originalSize',{width: dialogParent.width(),height: dialogParent.height()});
+					jQuery.data(win,'originalPosition',dialogParent.offset());
+					
+					dialogParent.width(jQuery(window).width());
+					dialogParent.height(jQuery(window).height());
+					dialogParent.offset({top:0,left:0});
+					
+				}else{
+					jQuery.data(win,'isMaximized',false);
+					var dialogParent = win.parent();					
+					var width  = jQuery.data(win,'originalSize').width;
+					var height = jQuery.data(win,'originalSize').height;					 
+					dialogParent.offset(jQuery.data(win,'originalPosition'));					
+					dialogParent.width(width);
+					dialogParent.height(height);
+				}
+				console.log(jQuery.data(win,'originalSize'))
+			})();
+						
+//			win.trigger({type:'updateIcons',action:'maximize'});
+			//custom event for maximize
+			win.trigger({type:'maximize'})
+		});
+	
 	},
 	
 	/**
