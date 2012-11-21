495a3a375ef7214d699390e44da9359842b2ea10
bogdan <bogdan.bugarschi@netapp.com>
2012-11-21 09:58:02 +0200

    Added Actions to Button and AnchorLinkControl

--- jWic/src/de/jwic/controls/AnchorLinkControl.java
+++ jWic/src/de/jwic/controls/AnchorLinkControl.java
@@ -4,11 +4,18 @@
  */
 package de.jwic.controls;
 
-import de.jwic.base.IControlContainer;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
+import de.jwic.base.IControlContainer;
+import de.jwic.ecolib.actions.Action;
+import de.jwic.ecolib.actions.IAction;
+import de.jwic.events.SelectionEvent;
+import de.jwic.events.SelectionListener;
 
 /**
  * Displays an anchor (&lt;a href="..."&gt;) link.
+ * 
  * @author Florian Lippisch
  * @version $Revision: 1.2 $
  */
@@ -19,7 +26,25 @@ public class AnchorLinkControl extends SelectableControl {
 	private String title = null;
 	private String infoMessage = null;
 
-	
+	private IAction action;
+
+	private final PropertyChangeListener propertyChangeListener = new PropertyChangeListener() {
+
+		@Override
+		public void propertyChange(PropertyChangeEvent evt) {
+			requireRedraw();
+		}
+	};
+
+	private final SelectionListener selectionListener = new SelectionListener() {
+
+		@Override
+		public void objectSelected(SelectionEvent event) {
+			onClick(event);
+		}
+
+	};
+
 	/**
 	 * @param container
 	 */
@@ -27,6 +52,7 @@ public class AnchorLinkControl extends SelectableControl {
 		super(container);
 		title = getName();
 	}
+
 	/**
 	 * @param container
 	 * @param name
@@ -34,43 +60,104 @@ public class AnchorLinkControl extends SelectableControl {
 	public AnchorLinkControl(IControlContainer container, String name) {
 		super(container, name);
 		title = name;
+
 	}
-	/* (non-Javadoc)
-	 * @see de.jwic.base.Control#actionPerformed(java.lang.String, java.lang.String)
+
+	/**
+	 * 
+	 * @param container
+	 * @param name
+	 * @param action
+	 */
+	public AnchorLinkControl(IControlContainer container, String name,
+			IAction action) {
+		this(container, name);
+		this.setAction(action);
+
+	}
+
+	/**
+	 * 
+	 * @param container
+	 * @param action
+	 */
+	public AnchorLinkControl(IControlContainer container, IAction action) {
+		this(container, null, action);
+	}
+
+	protected void onClick(SelectionEvent event) {
+		if (this.action != null) {
+			action.run();
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see de.jwic.base.Control#actionPerformed(java.lang.String,
+	 * java.lang.String)
 	 */
 	public void actionPerformed(String actionId, String parameter) {
-		
 		click();
-		
 	}
-	
+
+	/**
+	 * 
+	 * @param action
+	 *            the action to be set
+	 */
+	public void setAction(IAction action) {
+		this.action = action;
+
+		setTitle(action.getTitle());
+		setEnabled(action.isEnabled());
+		setVisible(action.isVisible());
+
+		action.addPropertyChangeListener(this.propertyChangeListener);
+		addSelectionListener(this.selectionListener);
+	}
+
+	/**
+	 * 
+	 * @return the action
+	 */
+	public IAction getAction() {
+		return action;
+	}
+
 	/**
 	 * @return Returns the title.
 	 */
 	public String getTitle() {
 		return title;
 	}
+
 	/**
-	 * @param title The title to set.
+	 * @param title
+	 *            The title to set.
 	 */
 	public void setTitle(String title) {
 		this.title = title;
 		setRequireRedraw(true);
 	}
-	
+
 	/**
 	 * @return Returns the infoMessage.
 	 */
 	public String getInfoMessage() {
 		return infoMessage;
 	}
+
 	/**
-	 * This text is displayed in the infobar of the browser during mouseover and as the
-	 * title of the anchor tag, wich results in a little popup info.
-	 * @param infoMessage The infoMessage to set.
+	 * This text is displayed in the infobar of the browser during mouseover and
+	 * as the title of the anchor tag, wich results in a little popup info.
+	 * 
+	 * @param infoMessage
+	 *            The infoMessage to set.
 	 */
 	public void setInfoMessage(String infoMessage) {
 		this.infoMessage = infoMessage;
 		setRequireRedraw(true);
 	}
+
 }
--- jWic/src/de/jwic/controls/Button.java
+++ jWic/src/de/jwic/controls/Button.java
@@ -4,15 +4,20 @@
  */
 package de.jwic.controls;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
 import de.jwic.base.IControlContainer;
 import de.jwic.base.ImageRef;
 import de.jwic.base.JavaScriptSupport;
-
+import de.jwic.ecolib.actions.IAction;
+import de.jwic.events.SelectionEvent;
+import de.jwic.events.SelectionListener;
 
 /**
- * Represents the &lt;button&gt; html element. A button displays text and/or
- * an image. 
- *  
+ * Represents the &lt;button&gt; html element. A button displays text and/or an
+ * image.
+ * 
  * @author Florian Lippisch
  * @version $Revision: 1.3 $
  */
@@ -27,7 +32,22 @@ public class Button extends SelectableControl {
 	private boolean submitButton = false;
 	private String tooltip = null;
 	private String confirmMsg = "";
-	
+	private IAction action;
+
+	private PropertyChangeListener propertyChangeListener = new PropertyChangeListener() {
+		public void propertyChange(PropertyChangeEvent evt) {
+			requireRedraw();
+		}
+	};
+
+	private final SelectionListener selectionListener = new SelectionListener() {
+
+		@Override
+		public void objectSelected(SelectionEvent event) {
+			onClick(event);
+		}
+	};
+
 	/**
 	 * @param container
 	 */
@@ -35,6 +55,7 @@ public class Button extends SelectableControl {
 		super(container);
 		init();
 	}
+
 	/**
 	 * @param container
 	 * @param name
@@ -43,7 +64,19 @@ public class Button extends SelectableControl {
 		super(container, name);
 		init();
 	}
-	/* (non-Javadoc)
+
+	public Button(IControlContainer container, String name, IAction action) {
+		this(container, name);
+		this.setAction(action);
+	}
+
+	public Button(IControlContainer container, IAction action) {
+		this(container, null, action);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see de.jwic.base.Control#init()
 	 */
 	private void init() {
@@ -52,32 +85,46 @@ public class Button extends SelectableControl {
 			title = getName();
 		}
 	}
-	
-	/* (non-Javadoc)
-	 * @see de.jwic.base.Control#actionPerformed(java.lang.String, java.lang.String)
+
+	/**
+	 * 
+	 * @param event
+	 */
+	protected void onClick(SelectionEvent event) {
+		if (action != null) {
+			this.action.run();
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see de.jwic.base.Control#actionPerformed(java.lang.String,
+	 * java.lang.String)
 	 */
 	public void actionPerformed(String actionId, String parameter) {
-		
 		click();
-		
 	}
-	
+
 	/**
 	 * @return Returns the title.
 	 */
 	public String getTitle() {
 		return title;
 	}
+
 	/**
-	 * @param title The title to set.
+	 * @param title
+	 *            The title to set.
 	 */
 	public void setTitle(String title) {
 		this.title = title;
 		setRequireRedraw(true);
 	}
-	
+
 	/**
 	 * Returns the icon for the button depending on the state.
+	 * 
 	 * @return
 	 */
 	public ImageRef getIcon() {
@@ -90,77 +137,128 @@ public class Button extends SelectableControl {
 
 	/**
 	 * Returns true if an icon is specified.
+	 * 
 	 * @return
 	 */
-	public boolean hasIcon(){
-		return iconEnabled != null; 
+	public boolean hasIcon() {
+		return iconEnabled != null;
 	}
 
 	/**
 	 * Returns true if the button should behave like a submit button.
+	 * 
 	 * @return boolean
 	 */
 	public boolean isSubmitButton() {
 		return submitButton;
 	}
+
 	/**
 	 * Set to true if the button should be of the type submit.
-	 * @param submitButton The submitButton to set.
+	 * 
+	 * @param submitButton
+	 *            The submitButton to set.
 	 */
 	public void setSubmitButton(boolean submitButton) {
 		this.submitButton = submitButton;
 		requireRedraw();
 	}
+
 	/**
 	 * @return Returns the tooltip.
 	 */
 	public String getTooltip() {
 		return tooltip;
 	}
+
 	/**
-	 * @param tooltip The tooltip to set.
+	 * @param tooltip
+	 *            The tooltip to set.
 	 */
 	public void setTooltip(String tooltip) {
 		this.tooltip = tooltip;
 		requireRedraw();
 	}
+
 	/**
 	 * @return Returns the confirmMsg.
 	 */
 	public String getConfirmMsg() {
 		return confirmMsg;
 	}
+
 	/**
-	 * @param confirmMsg The confirmMsg to set.
+	 * @param confirmMsg
+	 *            The confirmMsg to set.
 	 */
 	public void setConfirmMsg(String confirmMsg) {
 		this.confirmMsg = confirmMsg;
 		requireRedraw();
 	}
+
 	/**
 	 * @return the iconDisabled
 	 */
 	public ImageRef getIconDisabled() {
 		return iconDisabled;
 	}
+
 	/**
-	 * @param iconDisabled the iconDisabled to set
+	 * @param iconDisabled
+	 *            the iconDisabled to set
 	 */
 	public void setIconDisabled(ImageRef iconDisabled) {
 		this.iconDisabled = iconDisabled;
 		requireRedraw();
 	}
+
 	/**
 	 * @return the iconEnabled
 	 */
 	public ImageRef getIconEnabled() {
 		return iconEnabled;
 	}
+
 	/**
-	 * @param iconEnabled the iconEnabled to set
+	 * @param iconEnabled
+	 *            the iconEnabled to set
 	 */
 	public void setIconEnabled(ImageRef iconEnabled) {
 		this.iconEnabled = iconEnabled;
 		requireRedraw();
+	}
+
+	/**
+	 * 
+	 * @param action
+	 *            the action to be set
+	 */
+	public void setAction(IAction action) {
+		this.action = action;
+		setTitle(action.getTitle());
+		setIconEnabled(action.getIconEnabled());
+		setIconDisabled(action.getIconDisabled());
+		setEnabled(action.isEnabled());
+		setVisible(action.isVisible());
+		setTooltip(action.getTooltip());
+
+		action.addPropertyChangeListener(this.propertyChangeListener);
+		addSelectionListener(selectionListener);
+	}
+
+	/**
+	 * 
+	 * @return the action
+	 */
+	public IAction getAction() {
+		return action;
+	}
+
+	@Override
+	public void click() {
+		super.click();
+		if (action != null) {
+			action.run();
+		}
 	}
 }
