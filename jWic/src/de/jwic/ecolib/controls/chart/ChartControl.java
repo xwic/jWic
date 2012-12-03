96b7b04e837bd528836953691b8d532556beca23
bogdan <bogdan.bugarschi@netapp.com>
2012-12-03 09:36:16 +0200

    fix ChartControl

--- jWic/src/de/jwic/ecolib/controls/chart/ChartControl.java
+++ jWic/src/de/jwic/ecolib/controls/chart/ChartControl.java
@@ -52,7 +52,7 @@ import de.jwic.controls.ImageControl;
 public class ChartControl extends ImageControl implements Serializable {
 
 	protected JFreeChart chart = null;
-	protected long rendered = 0;
+	//protected long rendered = 0;
 	
 	/**
 	 * Constructor.
@@ -71,14 +71,14 @@ public class ChartControl extends ImageControl implements Serializable {
 		super(container, name);
 	}
 	
-	/* (non-Javadoc)
+	/* (non-Javadoc)e
 	 * @see de.jwic.controls.ImageControl#getFilename()
 	 */
 	public String getFilename() {
 		if (chart == null) {
 			return getName();
 		}
-		return getName() + "_" + chart.getTitle() + "(" + rendered++ + "," + System.currentTimeMillis() + ").png";				
+		return getName() + "_" + chart.getTitle().getText().replace(' ', '_')+ ".png";				
 	}
 
 	/* (non-Javadoc)
