8b8460b01caccbffe5feee4daf361b297d119af4
Florian Lippisch <lippisch@netapp.com>
2013-04-25 11:45:24 +0200

    AnchorLink now supports css class names; optimized toolBar layout

--- jWic/src_core/de/jwic/controls/ToolBarGroup.java
+++ jWic/src_core/de/jwic/controls/ToolBarGroup.java
@@ -21,9 +21,6 @@ package de.jwic.controls;
 
 import de.jwic.base.ControlContainer;
 import de.jwic.base.IControlContainer;
-import de.jwic.controls.AnchorLink;
-import de.jwic.controls.Button;
-import de.jwic.controls.LabelControl;
 import de.jwic.controls.actions.ActionAnchorLink;
 import de.jwic.controls.actions.ActionButton;
 import de.jwic.controls.actions.IAction;
@@ -78,7 +75,7 @@ public class ToolBarGroup extends ControlContainer {
 	 */
 	public AnchorLink addActionAnchorLink(IAction action){
 		AnchorLink anchorLinkControl = new ActionAnchorLink(this, action);
-		
+		anchorLinkControl.setCssClass("j-toolbar-anchor");
 		return anchorLinkControl;
 	}
 	
@@ -99,8 +96,8 @@ public class ToolBarGroup extends ControlContainer {
 	 * @param label
 	 * @return
 	 */
-	public LabelControl addLabel(String label) {
-		LabelControl lbl= new LabelControl(this);
+	public Label addLabel(String label) {
+		Label lbl= new Label(this);
 		lbl.setText(label);
 		lbl.setCssClass("j-toolbar-label");
 		return lbl;
