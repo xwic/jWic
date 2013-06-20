f3405d1c3cb2bde647d4bee00887b835947e4030
bogdan <Bogdan.Bugarschi@netapp.com>
2013-06-20 13:53:15 +0300

    Changed Button vtl and js for new ui update

--- jWic/src_controls/de/jwic/controls/Button.js
+++ jWic/src_controls/de/jwic/controls/Button.js
@@ -22,6 +22,21 @@
 						at: "center bottom"
 						}});
 			#end
+			#if($control.hasIcon())
+				btn.removeClass('ui-button-text-only').removeClass('ui-button-text-icon-secondary');
+				if(btn.text()===''){
+					btn.addClass('ui-button-icon-only');
+				}else{
+					btn.addClass('ui-button-text-icon');
+				}
+			btn.append('<span class="j-button-icon"><img border="0" src="$control.icon.path"></span>');
+			#end
+			if(btn.find('.ui-icon.ui-icon-triangle-1-s').length!==0){
+				btn.removeClass('ui-button-text-only').removeClass('ui-button-icon-only').removeClass('ui-button-text-icon').addClass('ui-button-text-icons');
+			};
+			
+			
+			
 		}
 	#end
 	}
