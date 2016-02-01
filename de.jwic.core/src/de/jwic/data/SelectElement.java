955f1ebb6e606493b0e57e2d9c354f4cdcb13c00
Florian Lippisch <florian.lippisch@netapp.com>
2016-02-01 15:14:56 +0100

    Added hashCode() and equals(..) to ImageRef and SelectElement

--- de.jwic.core/src/de/jwic/data/SelectElement.java
+++ de.jwic.core/src/de/jwic/data/SelectElement.java
@@ -134,5 +134,53 @@ public class SelectElement implements ISelectElement, Serializable {
 		sb.append("title: " + title);
 		return sb.toString();
 	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((image == null) ? 0 : image.hashCode());
+		result = prime * result + ((key == null) ? 0 : key.hashCode());
+		result = prime * result + (selectable ? 1231 : 1237);
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SelectElement other = (SelectElement) obj;
+		if (image == null) {
+			if (other.image != null)
+				return false;
+		} else if (!image.equals(other.image))
+			return false;
+		if (key == null) {
+			if (other.key != null)
+				return false;
+		} else if (!key.equals(other.key))
+			return false;
+		if (selectable != other.selectable)
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
+	}
+	
+	
 	
 }
