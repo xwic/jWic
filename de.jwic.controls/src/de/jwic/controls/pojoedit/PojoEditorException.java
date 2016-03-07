c6f98c5622b27c29824277264dc0fa4d62810303
Otto <dotto@netapp.com>
2016-03-07 10:37:08 +0100

    remove c'tor

--- de.jwic.controls/src/de/jwic/controls/pojoedit/PojoEditorException.java
+++ de.jwic.controls/src/de/jwic/controls/pojoedit/PojoEditorException.java
@@ -21,17 +21,6 @@ public class PojoEditorException extends Exception {
 	/**
 	 * @param message
 	 * @param cause
-	 * @param enableSuppression
-	 * @param writableStackTrace
-	 */
-	public PojoEditorException(String message, Throwable cause,
-			boolean enableSuppression, boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-	}
-
-	/**
-	 * @param message
-	 * @param cause
 	 */
 	public PojoEditorException(String message, Throwable cause) {
 		super(message, cause);
